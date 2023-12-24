d63581136a79f76410d2bfbbc320a7a7d020cbf7
ahmedhabeeb73 <alnaib888@gmail.com>
2023-12-24 16:58:46 +0100

    clean the router

--- public/index.php
+++ public/index.php
@@ -2,23 +2,9 @@
 require '../helpers.php';
 
 
-$routes = [
-   
-        '/jobplatform/public/' => 'controllers/home.php',
-        '/jobplatform/public/listings' => 'controllers/listings/index.php',
-        '/jobplatform/public/listings/create' => 'controllers/listings/create.php',
-        '404' => 'controllers/error/404.php',
-    ];
-    
-
-
-
 $uri=$_SERVER['REQUEST_URI'];
 
-if(array_key_exists($uri, $routes)){
-    require(base_path($routes[$uri]));
 
-}else{
-    require(base_path($routes['404']));
 
-}
+require base_path('router.php');;
+
---
+++ routes.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+   
+    '/jobplatform/public/' => 'controllers/home.php',
+    '/jobplatform/public/listings' => 'controllers/listings/index.php',
+    '/jobplatform/public/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php',
+];
