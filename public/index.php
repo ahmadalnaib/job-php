90fc9fc59de8e9c1b4b17e1c0f4d49a760f4d828
ahmedhabeeb73 <alnaib888@gmail.com>
2023-12-24 19:55:43 +0100

    refactor to class router

--- public/index.php
+++ public/index.php
@@ -1,10 +1,15 @@
 <?php
 require '../helpers.php';
 
+require base_path('Router.php');
+
+$router=new Router();
+
+$routes=require base_path('routes.php');
 
 $uri=$_SERVER['REQUEST_URI'];
+$method=$_SERVER['REQUEST_METHOD'];
 
 
 
-require base_path('router.php');;
-
+$router->route($uri,$method);
--- routes.php
+++ routes.php
@@ -1,9 +1,5 @@
 <?php
 
-return [
-   
-    '/jobplatform/public/' => 'controllers/home.php',
-    '/jobplatform/public/listings' => 'controllers/listings/index.php',
-    '/jobplatform/public/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php',
-];
+$router->get('/jobplatform/public/','controllers/home.php');
+$router->get('/jobplatform/public/listings','controllers/listings/index.php');
+$router->get('/jobplatform/public/listings/create','controllers/listings/create.php');
