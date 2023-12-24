1786c2ea5dc28ecc51969c8a27d1637dd858a217
ahmedhabeeb73 <alnaib888@gmail.com>
2023-12-24 12:25:02 +0100

    create router

--- public/index.php
+++ public/index.php
@@ -1,3 +1,24 @@
 <?php
 require '../helpers.php';
-loadView('home');
+
+
+$routes = [
+   
+        '/jobplatform/public/' => 'controllers/home.php',
+        '/jobplatform/public/listings' => 'controllers/listings/index.php',
+        '/jobplatform/public/listings/create' => 'controllers/listings/create.php',
+        '404' => 'controllers/error/404.php',
+    ];
+    
+
+
+
+$uri=$_SERVER['REQUEST_URI'];
+
+if(array_key_exists($uri, $routes)){
+    require(base_path($routes[$uri]));
+
+}else{
+    require(base_path($routes['404']));
+
+}
